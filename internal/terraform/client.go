97a3147bb1eb2263f57f5555b504c052ee9b627e
Lucas Vieira <vieiralucas4@gmail.com>
2023-08-05 20:07:44 -0700

    remove unnecessary packages after moving to tfexec

--- internal/terraform/client.go
+++
@@ -1,7 +0,0 @@
-package terraform
-
-type Client interface {
-	Init(dir string) error
-	Apply(dir string, state *State) (*State, error)
-	Destroy(dir string, state *State, target ...string) (*State, error)
-}
