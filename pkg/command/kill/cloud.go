92603c5a7668a40b0eb3ce40569197fc4fbaee1e
João Augusto Lissoni Lanjoni <joaoaugustolanjoni@gmail.com>
2023-09-16 22:07:14 -0300

    fix: change alert message to cloud kill

--- pkg/command/kill/cloud.go
+++ pkg/command/kill/cloud.go
@@ -96,7 +96,7 @@ func (e *cloudKillCommand) Run(
 	if hasDependants {
 		s.Error()
 		sm.Stop()
-		return errors.New("can't kill this layer because other layers depend on it\nuse the --force flag to kill it anyway")
+		return errors.New("can't kill this layer because other layers depend on it")
 	}
 
 	url := fmt.Sprintf("/v1/definitions/%s/instances/%s/kill", definitionName, instanceName)
